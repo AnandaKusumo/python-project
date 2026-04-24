fba809cc650dab59036ec39276f1d1c727df5ce6
AnandaKusumo <anandakusuma232@gmail.com>
2026-04-24 11:24:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    $sisi="50";
+
+$hitungpersegi= 4 * "$sisi";
+// echo "$hitung";
+?>
+
+<html>
+    <head>
+        <body>
+            <table>
+                <div>
+                    <h1>Menghitung Luas persegi</h1>
+                    Input Sisi <input type="text" name="sisi">
+                </div><br>
+
+                <div>
+                    <input type="submit" value="hitung">
+                </div>
+            </table>
+        </body>
+    </head>
+</html>
